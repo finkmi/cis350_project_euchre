3f23c4eceabc4f9e887649e7f20bbd8ed591c1cc
charliedorn6 <charliedorn6@gmail.com>
2020-07-09 19:47:06 -0400

    Added comments to EuchreView class

--- src/cis350_project_euchre/EuchreView.java
+++ src/cis350_project_euchre/EuchreView.java
@@ -2,19 +2,35 @@ package cis350_project_euchre;
 import java.awt.Dimension;
 import javax.swing.*;
 
-
+/**********************************************************************
+ * The view class for the Euchre program.
+ * 
+ * @author Michael Fink, Charlie Dorn
+ *********************************************************************/
 public class EuchreView {
 	
+	/******************************************************************
+	 * The main method for the Euchre program. Sets up the GUI for the
+	 * Euchre game and creates an instance of the euchreController
+	 * used to run the euchre game.
+	 * 
+	 * @param args
+	 *****************************************************************/
     public static void main (String[] args) {
     	
+    	/* Create the JFrame for the game, make it exit when the close
+    	 * button is pressed */
     	JFrame frame = new JFrame ("Euchre");
         frame.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
         EuchreController controller = new EuchreController();
         
+        /* Add the controller to the JFrame created for the program */
         frame.getContentPane().add(controller);
         frame.setResizable(true);
-        //frame.setPreferredSize(new Dimension(800, 637));
+        
+        /* Pack to good dimensions for the game */
         frame.pack();
+        /* Set the frame visible so it can be interacted with */
         frame.setVisible(true);
     }
 }
